dd543ed82461cac52e94e561dae6bd385c80126f
Pudim <guipugin@gmail.com>
2015-08-21 12:26:28 -0300

    Atualização de tabelas com SERIAL

---
+++ bluesoft_agenda.sql
@@ -0,0 +1,12 @@
+﻿create table sala
+(
+   sala_id serial primary key not null,
+   sala_nome varchar(255)
+);
+
+create table agendamento
+(
+   ag_id serial not null,
+   ag_data timestamp,
+   ag_salaId int references sala(sala_id)
+);
--- src/main/java/br/com/bluesoft/agenda/view/Principal.java
+++ src/main/java/br/com/bluesoft/agenda/view/Principal.java
@@ -4,7 +4,6 @@ public class Principal {
 
 	public static void main(String[] args) {
 		String sala;
-		
 	}
 
 }
