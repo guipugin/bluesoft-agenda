a353359624ca303e6b2323d9abcbfc7ba93083be
Pudim <guipugin@gmail.com>
2015-08-21 16:02:22 -0300

    Atualização da interface Connection

--- src/main/java/br/com/bluesoft/agenda/interfaces/Connection.java
+++ src/main/java/br/com/bluesoft/agenda/interfaces/Connection.java
@@ -4,17 +4,34 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
-import javax.naming.spi.DirStateFactory.Result;
+import javax.swing.JOptionPane;
 
 public interface Connection {
-	Result executeQuery(String sql);
+	final String url = "jdbc:postgresql://localhost:8080/bluesoft-agenda";
+	final String usuario = "Pudim";
+	final String senha = "123456";
+	final String driver = "org.postgresql.Driver";
+	Connection con = null;
 
-	public static Connection getConnection() throws SQLException {
-		Connection con = new Connection("jdbc:postgresql://localhost/bluesoft-agenda", "Pudim", "123456");
+	public static Connection conecta() {
+		try {
+			Class.forName(driver);
+		} catch (ClassNotFoundException cnfe) {
+			JOptionPane.showMessageDialog(null, "não encontrado");
+			System.out.println("Driver não encontrado!!");
+			cnfe.printStackTrace();
+		}
+		try {
+			con = DriverManager.getConnection(url, usuario, senha);
+			System.out.println("Conexão realizada com sucesso!");
+		} catch (SQLException se) {
+			System.out.println("Não foi possivel conectar.");
+			se.printStackTrace();
+		}
 		return con;
 	}
 
-	public void closeConnection() {
+	public static void closeConnection() {
 		try {
 			con.close();
 		} catch (SQLException e) {
